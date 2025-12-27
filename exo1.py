1bd5f43dd2819d1f96635dada2df44a22f4917a7
imad12345 <bimh1997@gmail.com>
2025-12-27 10:24:44 +0100

    exo 1 added

--- exo1.py
+++ exo1.py
@@ -2,3 +2,28 @@ print('hello world!')
 
 def hello():
     return 0
+
+def add(a,b):
+    return a+b
+
+def multiply(a,b):
+    return a*b
+
+def find_max_value(d):
+    return max(d.values())
+
+# Initialize dictionary
+my_dict = {
+    'a': 10,
+    'b': 25,
+    'c': 7,
+    'd': 18
+}
+
+vv = my_dict.values()
+
+# Find maximum value
+max_value = find_max_value(my_dict)
+
+
+
